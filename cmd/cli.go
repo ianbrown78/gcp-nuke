41e76a38a751072b7af4cb701b99714e6138f6a9
Ian Brown <ian.brown@kasna.com.au>
2023-02-27 08:42:02 +1100

    Be more explicit with flags for our command.

--- cmd/cli.go
+++ cmd/cli.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/ianbrown78/gcp-nuke/config"
-	"github.com/ianbrown78/gcp-nuke/gcp"
+	"github.com/ianbrown78/gcp-nuke/resources"
 	"github.com/urfave/cli/v2"
 )
 
@@ -15,7 +15,7 @@ func Command() {
 	app := &cli.App{
 		Usage:     "The GCP project cleanup tool with added radiation",
 		Version:   "v0.1.0",
-		UsageText: "e.g. gcp-nuke --project test-nuke-262510 --dryrun",
+		UsageText: "e.g. resources-nuke --project resources-nuke-test --dryrun",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:     "project, p",
@@ -23,39 +23,43 @@ func Command() {
 				Required: true,
 			},
 			&cli.BoolFlag{
-				Name:  "no-dryrun, d",
-				Usage: "Do not perform a dryrun",
+				Name:     "no-dryrun, d",
+				Usage:    "Do not perform a dryrun",
+				Required: false,
 			},
 			&cli.IntFlag{
-				Name:  "timeout, t",
-				Value: 400,
-				Usage: "Timeout for removal of a single resource in seconds",
+				Name:     "timeout, t",
+				Value:    400,
+				Usage:    "Timeout for removal of a single resource in seconds",
+				Required: false,
 			},
 			&cli.IntFlag{
-				Name:  "polltime, p",
-				Value: 10,
-				Usage: "Time for polling resource deletion status in seconds",
+				Name:     "polltime, pt",
+				Value:    10,
+				Usage:    "Time for polling resource deletion status in seconds",
+				Required: false,
 			},
 			&cli.BoolFlag{
-				Name:  "keep-project, k",
-				Usage: "Keep the project. Just delete its resources.",
+				Name:     "no-keep-project, k",
+				Usage:    "Do not keep the project. Delete it with its resources.",
+				Required: false,
 			},
 		},
 		Action: func(c *cli.Context) error {
-
 			// Behaviour to delete all resource in parallel in one project at a time - will be made into loop / concurrenct project nuke if required
 			config := config.Config{
-				Project:     c.String("project"),
-				NoDryRun:    c.Bool("no-dryrun"),
-				Timeout:     c.Int("timeout"),
-				PollTime:    c.Int("polltime"),
-				KeepProject: c.Bool("keep-project"),
-				Context:     gcp.Ctx,
-				Zones:       gcp.GetZones(gcp.Ctx, c.String("project")),
-				Regions:     gcp.GetRegions(gcp.Ctx, c.String("project")),
+				Project:       c.String("project"),
+				NoDryRun:      c.Bool("no-dryrun"),
+				Timeout:       c.Int("timeout"),
+				PollTime:      c.Int("polltime"),
+				NoKeepProject: c.Bool("no-keep-project"),
+				Context:       resources.Ctx,
+				Zones:         resources.GetZones(resources.Ctx, c.String("project")),
+				Regions:       resources.GetRegions(resources.Ctx, c.String("project")),
 			}
+
 			log.Printf("[Info] Timeout %v seconds. Polltime %v seconds. Dry run: %v", config.Timeout, config.PollTime, config.NoDryRun)
-			gcp.RemoveProject(config)
+			resources.RemoveProjectResources(config)
 
 			return nil
 		},
